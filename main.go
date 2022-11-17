bad3328d68b8eeb8213924c4e4a418b0864bc78f
AntonyIS <antonyshikubu@gmail.com>
2022-11-17 17:41:07 +0300

    adding Dockerfile and docker-compose.yml file

--- main.go
+++ main.go
@@ -8,9 +8,7 @@ import (
 
 func main() {
 	fmt.Println("Welcome to the Modular Art API ...")
-	// app := fiber.New()
 
-	// routes.SetupFiberRoutes(app)
 	router := routes.SetupGinRouter()
 
 	router.Run(":5000")
