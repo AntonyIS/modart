3abfb6d85310d9887c6420bc8c803d01581bedb9
AntonyIS <antonyshikubu@gmail.com>
2022-11-26 20:24:04 +0300

    adding text in client in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h1>Welcome to Mod Art</h1>
+        <p>
+        At the end of this tutorial, you'll have a Docker container running your app that you can deploy as you see fit 👌
+        </p>
       </header>
     </div>
   );
